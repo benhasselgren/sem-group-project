381a114ecc022753f0d358d2540276778cb8c6d0
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-04 13:44:16 +0000

    Removed mongodb imports and code to makeway for sql code

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,10 +1,5 @@
 package com.napier.sem;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoCollection;
-import org.bson.Document;
-
 public class App
 {
     public static void main(String[] args)

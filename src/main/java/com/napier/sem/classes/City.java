6c431a1c098b97f3d2e7cf92acae7a69b70faf6e
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-25 10:32:12 +0000

    Added country class to store details about countries

--- src/main/java/com/napier/sem/classes/City.java
+++ src/main/java/com/napier/sem/classes/City.java
@@ -1,4 +1,13 @@
 package com.napier.sem.classes;
 
 public class City {
+    /**
+     * Instance variables.
+     */
+    private int id;
+    private String name;
+    private String countryCode;
+    private String district;
+    private int population;
+
 }

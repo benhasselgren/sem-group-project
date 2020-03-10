58dd89e3c76e8a97ed7786190713652d4fc9223e
Ben Hasselgren <ben.nadalito@gmail.com>
2020-03-10 13:19:00 +0000

    Added integration tests file and and updated travis.yml to run that file

---
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -0,0 +1,46 @@
+package com.napier.sem;
+
+import com.napier.sem.classes.City;
+import com.napier.sem.classes.Country;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppIntegrationTest
+{
+    // ------------------------------------- Initialise -------------------------------------
+    static App app;
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+        app.connect("localhost:33060");
+    }
+
+    // ------------------------------------- Test testGetCountry() -------------------------------------
+    @Test
+    void testGetCountry()
+    {
+        Country country = app.getCountry("Argentina");
+        assertEquals(country.getCode(), "ARG");
+        assertEquals(country.getName(), "Argentina");
+        assertEquals(country.getContinent(), "South America");
+        assertEquals(country.getRegion(), "South America");
+        assertEquals(country.getSurfaceArea(), 2780400.00);
+        assertEquals(country.getIndepYear(), 1816);
+        assertEquals(country.getPopulation(), 37032000);
+        assertEquals(country.getLifeExpectancy(), 75.1);
+        assertEquals(country.getLifeExpectancy(), 75.1);
+        assertEquals(country.getGnp(), 340238.00);
+        assertEquals(country.getGnpold(), 323310.00);
+        assertEquals(country.getLocalName(), "Argentina");
+        assertEquals(country.getGovernmentForm(), "Federal Republic");
+        assertEquals(country.getHeadOfState(), "Fernando de la Rúa");
+        assertEquals(country.getCapitalCity().getId(), 69);
+    }
+}
